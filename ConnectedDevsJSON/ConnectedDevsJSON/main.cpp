7e0acfe7882409472afd717116c29b8385b4f3d7
elhayr1 <elhayr1@gmail.com>
2016-10-15 19:40:54 -0700

    added: writing to file

--- ConnectedDevsJSON/ConnectedDevsJSON/main.cpp
+++ ConnectedDevsJSON/ConnectedDevsJSON/main.cpp
@@ -1,23 +1,48 @@
+//-------------------------
+//Created by: Elchay Rauper
+//-------------------------
+
+#include <iostream>
+#include <fstream>
 #include "devices_tree.h"
 #include "JSON.h"
 
+//edit next lines in order to print to file
+#define PRINT_TO_FILE true
+#define FILE_PATH "output_file.txt"
+
 int main()
 {
+	
+	//get data structure ready
 	DevicesTree tree;
 	tree.feedTree();
-	//tree.printTree(tree.root());
-	//std::string jsonOut;
+
+	//enable this line to print devices tree as raw data
+	//tree.printTree(tree.root()); 
+	
 	JSONObject jsonOut;
 	tree.devicesTreeToJSON(tree.root(), jsonOut);
-	//std::cout << jsonOut << "\n\n";
-
-    /*
-	std::wstring convertor = std::wstring(jsonOut.begin(), jsonOut.end());
-	const wchar_t* widecstr = convertor.c_str();
-	JSONValue *value = JSON::Parse(widecstr);
-	std::wcout << value->Stringify(true).c_str();*/
+	
 	JSONValue *out = new JSONValue(jsonOut);
-	std::wcout << out->Stringify(true).c_str();
+
+	if (PRINT_TO_FILE)
+	{
+		std::wofstream outFile;
+		outFile.open(FILE_PATH);
+		if (outFile.is_open())
+		{
+			outFile << out->Stringify(true).c_str();
+			if (!outFile.good())
+				std::cout << "\nSome error ocurred while trying to write to file.\n";
+			else
+				std::cout << "\nWriting connected file to file ended successfuly\n";
+		}
+		else
+			std::cout << "Error: can't open file at " << FILE_PATH << "\nCheck file path.\n";
+	}
+	else
+		std::wcout << out->Stringify(true).c_str();
 	
 	return 0;
 }
